2910c113d1ddf952ef155ea66ef115c216ac3fce
Valerie Osei <valerie.osei@gmail.com>
2018-07-23 15:58:51 -0400

    added steps 3 to 6 in cats.rb

--- cats.rb
+++ cats.rb
@@ -1,30 +1,53 @@
 
 class Cat
 
-def initialize (name, preferred_food, meal_time)
-  @name = name
-  @preferred_food = preferred_food
-  @meal_time = meal_time
+  def initialize (name, preferred_food, meal_time, preferred_game)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+    @preferred_game = preferred_game
+  end
+
+  def eats_at
+    if @meal_time <12
+      return "#{@meal_time} am"
+    else
+      return "#{@meal_time} pm"
+    end
+  end
+
+  def meow
+    return "My name is #{@name} and I eat #{@preferred_food} at #{eats_at}, and my favorite game is #{@preferred_game}"
+  end
+
+  def preferred_game=(preferred_game)
+    @preferred_game = preferred_game
+  end
+
+  def preferred_game
+    return @preferred_game
+  end
+
+
+
 end
 
-def eats_at=(eats_at)
-  @eats_at = eats_at
-end
+pet = Cat.new('Bobby', 'Fancy feast', 13, 'fetch')
+pet2 = Cat.new('Sasha', 'Fish', 18, 'scratch')
+#
+# puts "#{pet}"
+# puts pet2
+#
+# time = Cat.new('11am')
+#
+# puts time
 
-def eats_at
-  return @eats_at
-end
+puts pet.eats_at
 
+puts pet.meow
 
+puts pet2.meow
 
-end
+puts pet.preferred_game = "Jump" # --> changing the value set when method is called will require a getter to retrieve new info
 
-pet = Cat.new('Bobby', 'Fancy feast', 'lunch')
-pet2 = Cat.new('Sasha', 'Fish', 'dinner')
-
-puts "#{pet}"
-puts pet2
-
-time = Cat.new('11am')
-
-puts time
+puts pet.meow
