68d70ed3dd0ecc48163224018cc9584d3f3d1712
Valerie Osei <valerie.osei@gmail.com>
2018-07-25 14:14:52 -0400

    created hash, updated with name of committee members and their responsiblities

---
+++ re-inforcing_exercise2.rb
@@ -0,0 +1,48 @@
+
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very important Project",
+  due_date: "December 14, 2019",
+  id: "3284",
+
+  steps: [{person: "Stella",
+          description: "conduct of interviews",
+          due_date: "August 1, 2018"
+          },
+          {person: "Salma",
+          description: "code of conduct",
+          due_date: "January 1, 2018"
+          },
+          {person: "Kai",
+          description: "compile results",
+          due_date: "November 10, 2018"
+          },
+          {person: "Stella",
+          description: "version 1",
+          due_date: "January 15, 2019"
+          },
+          {person: "Salma",
+          description: "revisions",
+          due_date: "March 30, 2019"
+          },
+          {person: "Kai",
+          description: "version 2",
+          due_date: "July 12, 2019"
+          },
+          {person: "Stella",
+          description: "final edit",
+          due_date: "October 1, 2019"
+          },
+          {person: "Salma",
+          description: "final version",
+          due_date: "November 20, 2019"
+          },
+          {person: "Kai",
+          description: "Wrap up",
+          due_date: "December 1, 2019"
+          }
+
+        ]
+      }
+
+      puts project
